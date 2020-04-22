bd14aa11921ef9fd67fc053eddc0de727544899f
Sam White <samueljhwhite@gmail.com>
2020-04-22 18:11:16 +0200

    debug axios deployment issues

--- client/src/components/TaskView.js
+++ client/src/components/TaskView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios'.default;
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 import TaskName from './task-view/TaskName.js';
